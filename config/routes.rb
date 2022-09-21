f075873f8239d928a8d7d619ffa562bc808ed49f
Colin <cdodowdee@gmail.com>
2022-09-21 09:29:29 -0400

    Added sessions views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: "users#new", as: 'signup'
+  get 'login', to: "sessions#new", as: 'login'
+  get 'logout', to: "sessions#destroy", as: 'logout'
 end
