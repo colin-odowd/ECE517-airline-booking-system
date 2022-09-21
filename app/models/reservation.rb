5230d04f229be37d5bf02f8032585e252a903d39
Colin <cdodowdee@gmail.com>
2022-09-21 19:31:11 -0400

    Fixed enum in reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,6 @@ class Reservation < ApplicationRecord
     belongs_to :flight
     has_many :baggages
 
-    enum :class [ :class_first, :class_business, :class_economy ]
-    enum :amenities [ :amenities_none, :amenities_wifi, :amenities_meal_preference, :amenities_extra_legroom ]
+    enum class: [ :class_first, :class_business, :class_economy ]
+    enum amenities: [ :amenities_none, :amenities_wifi, :amenities_meal_preference, :amenities_extra_legroom ]
 end
