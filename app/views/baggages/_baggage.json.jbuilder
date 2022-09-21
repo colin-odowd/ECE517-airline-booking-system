fa0992e42f6edab6510ef519853f50ec23929efe
bbdavis4 <bbdavis4@ncsu.edu>
2022-09-20 21:14:16 -0400

    Add baggage

---
+++ app/views/baggages/_baggage.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! baggage, :id, :baggage_id, :weight, :cost, :created_at, :updated_at
+json.url baggage_url(baggage, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :homes
+  resources :baggages
   resources :reservations
   resources :flights
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
