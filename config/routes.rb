95ff1e22f24632d40d0323098d503b1090656ed7
bbdavis4 <bbdavis4@ncsu.edu>
2022-09-20 21:46:31 -0400

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :homes
+  get 'welcome/index'
   resources :baggages
   resources :reservations
   resources :flights
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
