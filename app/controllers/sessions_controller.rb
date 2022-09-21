07c766c7dcf7317eb8bd0f2896d0a6c15f065c2c
Colin <cdodowdee@gmail.com>
2022-09-21 19:11:26 -0400

    Fixed session controller login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email_address(params[:email_address])
+    user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url
