1dc5c2c0368ec9ab0efe0e10ca4f34b9b92029f7
Colin <cdodowdee@gmail.com>
2022-09-21 20:51:50 -0400

    Enum dropdown for Flight views

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ApplicationRecord
     has_many :reservations, dependent: :delete_all
 
-    enum klass: [ :class_jumbo, :class_private, :class_small ]
+    enum flight_class: [ :class_jumbo, :class_private, :class_small ]
     enum status: [ :status_available, :status_complete ]
 end
