78f9d23501ef7ba635fd3a57273651a0b722a405
btbd <b4davis88@gmail.com>
2022-09-21 20:30:46 -0400

    Change class to klassg

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ApplicationRecord
     has_many :reservations
 
-    enum class: [ :class_jumbo, :class_private, :class_small ]
+    enum klass: [ :class_jumbo, :class_private, :class_small ]
     enum status: [ :status_available, :status_complete ]
 end
