c7e91c63e061e87c900229c95369ca410f1079b4
naydt01 <58788092+naydt01@users.noreply.github.com>
2022-09-27 18:14:47 -0400

    Fix Names of classes and status

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ApplicationRecord
     has_many :reservations, dependent: :delete_all
 
-    enum flight_class: [ :class_jumbo, :class_private, :class_small ]
-    enum status: [ :status_available, :status_complete ]
+    enum flight_class: [ :jumbo, :private_, :small ]
+    enum status: [ :available, :complete ]
 end
