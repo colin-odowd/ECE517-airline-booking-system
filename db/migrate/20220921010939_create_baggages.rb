b5a4765b33c7896e39eed6dd0bf43dcb67f01741
bbdavis4 <bbdavis4@ncsu.edu>
2022-09-27 17:39:54 -0400

    Delete baggage ID

--- db/migrate/20220921010939_create_baggages.rb
+++ db/migrate/20220921010939_create_baggages.rb
@@ -1,13 +1,11 @@
 class CreateBaggages < ActiveRecord::Migration[7.0]
   def change
     create_table :baggages do |t|
-      t.string :baggage_id
       t.integer :weight
       t.integer :cost
 
       t.timestamps
     end
-    add_index :baggages, :baggage_id, unique: true
     add_reference :baggages, :user, null: false, foreign_key: true
     add_reference :baggages, :reservation, null: false, foreign_key: true
   end
