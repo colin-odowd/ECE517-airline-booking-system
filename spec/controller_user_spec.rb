afaf0de59921ed7e4424ab5ebe5df484d7fd041c
bbdavis4 <bbdavis4@ncsu.edu>
2022-09-29 16:21:26 -0400

    Add GET user test

--- spec/controller_user_spec.rb
+++ spec/controller_user_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET index" do
-    current_user = User.find_by_admin(true)
+  current_user = User.find_by_admin(true)
 
+  describe "GET index" do
     it "assigns @users" do
-      get(:index, session: { :user_id => current_user.id })
+      get(:index, session: { user_id: current_user.id })
       expect(assigns(:users)).to eq([current_user])
     end
 
     it "renders the index template" do
-      get(:index, session: { :user_id => current_user.id })
+      get(:index, session: { user_id: current_user.id })
       expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET user" do
+    it "renders the show template" do
+      get(:show, params: { id: current_user.id }, session: { user_id: current_user.id })
+      expect(response).to render_template("show")
     end
   end
 end
