781b2acdb60efbc90bfe2dbf4fbc80a547b3f973
naydt01 <58788092+naydt01@users.noreply.github.com>
2022-09-29 17:07:00 -0400

    Working on rpec

--- spec/controller_user_spec.rb
+++ spec/controller_user_spec.rb
@@ -21,4 +21,47 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to render_template("show")
     end
   end
+
+  describe "GET new" do
+    it "assigns a new user" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "GET edit" do
+    it "renders the edit template" do
+      get(:edit, params: { id: current_user.id }, session: { user_id: current_user.id })
+      expect(response).to render_template("edit")
+    end
+  end
+
+  describe "POST create" do
+    context "with valid attributes" do
+      it "creates a new user" do
+        expect{
+          post :create, user: User.attributes_for(:user)
+        }.to change(User,:user).by(1)
+      end
+      
+      it "redirects to the new contact" do
+        post :create, user: User.attributes_for(:user)
+        response.should redirect_to User.last
+      end
+    end 
+  end
+
+  describe "POST update" do
+    it "renders the update" do
+      get(:update, params: { id: current_user.id }, session: { user_id: current_user.id })
+      expect(response).to render_template("update")
+    end
+  end
+
+  describe "GET destroy user" do
+    it "renders the destroy" do
+      get(:destroy, session: { user_id: current_user.id })
+      expect(response).to render_template("destroy")
+    end
+  end
 end
