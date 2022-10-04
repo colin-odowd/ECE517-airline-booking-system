b6b473cbc6872fc386051f16b8f1088c205da506
bbdavis4 <bbdavis4@ncsu.edu>
2022-10-04 14:29:48 -0400

    Add more validations and search

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,9 @@ class Flight < ApplicationRecord
 
     enum flight_class: [ :'Jumbo', :'Private', :'Small' ]
 
+    validates :manufacturer, presence: true
+    validates :source, presence: true
+    validates :destination, presence: true
     validates :source, comparison: { other_than: :destination }
     validates :cost, comparison: { greater_than: 0 }
     validates :cost_by_baggage_weight, comparison: { greater_than: 0 }
